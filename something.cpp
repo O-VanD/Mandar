45bddb348d6107b6d0cdaeb34eec18887419989c
Admin <ovand2@ocdsb.ca>
2017-11-16 19:25:07 -0500

    IO Test

---
+++ something.cpp
@@ -0,0 +1,15 @@
+/*
+ * something.cpp
+ *
+ *  Created on: Oct 26, 2017
+ *      Author: programmer
+ */
+#include <iostream>
+
+using namespace std;
+
+int thing()
+{
+	cout << "Shrish is popping off" << endl;
+	return 0;
+}
