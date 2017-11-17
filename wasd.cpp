290fa8262334e16084d3904c561c9d6297a98368
Admin <ovand2@ocdsb.ca>
2017-11-16 19:27:23 -0500

    Text

---
+++ wasd.cpp
@@ -0,0 +1,18 @@
+/*
+ * wasd.cpp
+ *
+ *  Created on: Oct 26, 2017
+ *      Author: programmer
+ */
+#include <iostream>
+
+using namespace std;
+
+int d()
+{
+	string name;
+	cout << "What is my name?" << endl;
+	cin >> name;
+	cout << name << endl;
+	return 0;
+}
